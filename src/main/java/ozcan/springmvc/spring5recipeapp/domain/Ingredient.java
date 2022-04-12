12863ad2102fb2d4ac25a9c3faf32bffdfe2713f
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2022-04-12 17:45:21 +0300

    added Ingredient entity

---
+++ src/main/java/ozcan/springmvc/spring5recipeapp/domain/Ingredient.java
@@ -0,0 +1,46 @@
+package ozcan.springmvc.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    //private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+
+}
