edaccd92a337be5fadcb463b339fd8ce0e4fb690
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2022-04-12 17:56:32 +0300

    added UnitOfMeasure entity

--- src/main/java/ozcan/springmvc/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/ozcan/springmvc/spring5recipeapp/domain/Ingredient.java
@@ -13,7 +13,8 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -42,5 +43,19 @@ public class Ingredient {
         this.amount = amount;
     }
 
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
 
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
 }
